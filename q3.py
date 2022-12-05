d90134c3747c8651926812f1198a664a298586e8
sangjun-Park1208 <sangjun1997@naver.com>
2022-12-05 12:51:53 +0900

    add q3

--- q3.py
+++ q3.py
@@ -1,34 +1,30 @@
 import cv2 as cv
 
-def q2():
+def q3():
     src = cv.imread('./images/case3/img3_4.png', cv.IMREAD_GRAYSCALE)
     if src is None:
         print('Image load failed!')
         return
 
     # 양방향 필터
-    filtered = cv.bilateralFilter(src, -1, 70, 75)
+    filtered = cv.bilateralFilter(src, -1, 15, 15)
     cv.imshow('filtered', filtered)
 
-    # # 히스토그램 평활화
-    # equalization = cv.equalizeHist(filtered)
-    # cv.imshow('equalization', equalization)
+    # 히스토그램 스트레칭
+    gmin, gmax, _, _ = cv.minMaxLoc(filtered)
+    stretch = cv.convertScaleAbs(filtered, alpha=255.0/(gmax - gmin), beta=-gmin * 255.0/(gmax - gmin))
+    cv.imshow('stretch', stretch)
 
-    # 적응형 이진화
-    binarization = cv.adaptiveThreshold(filtered, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 33, 10)
+    # 이진화
+    _, binarization = cv.threshold(stretch, 185, 255, cv.THRESH_BINARY)
     cv.imshow('binarization', binarization)
 
     # 침식
     erode = cv.erode(binarization, None)
     erode = cv.erode(erode, None)
-    erode = cv.erode(erode, None)
-    # erode = cv.erode(erode, None)
-
     cv.imshow('erode', erode)
 
-    # erode = ~erode
-
-    contours, hierarchy = cv.findContours(erode, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv.findContours(erode, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     dst = cv.cvtColor(erode, cv.COLOR_GRAY2BGR)
 
     print(hierarchy)
@@ -46,9 +42,11 @@ def q2():
                 continue
             i += 1
             break
-        ans.append(addnum)
         cv.drawContours(dst, contours, idx, (0, 0, 255), 2, cv.LINE_8, hierarchy)
         idx = hierarchy[0, idx, 0]
+        if addnum == 0:
+            continue
+        ans.append(addnum)
 
     ans.sort()
     print(ans)
@@ -57,17 +55,4 @@ def q2():
     cv.waitKey()
     cv.destroyAllWindows()
 
-q2()
-
-# cnt, labels, stats, centroids = cv.connectedComponentsWithStats(binarization)
-# howmany = 0
-# for i in range(1, cnt):
-#     (x, y, w, h, area) = stats[i]
-#     if area < 50:
-#         continue
-#     pt1 = (x, y)
-#     pt2 = (x + w, y + h)
-#     howmany += 1
-#     cv.rectangle(dst, pt1, pt2, (0, 0, 255), 2)
-#
-# print('howmany: ', howmany)
+q3()
